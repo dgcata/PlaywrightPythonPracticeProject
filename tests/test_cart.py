607382708134cc9edda76afbbe55e71dc417e236
dgcata <dcatacutan@fullscale.ph>
2024-04-25 17:49:59 +0800

    implemented test_can_logout_from_cart_page

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -8,6 +8,21 @@ def test_successful_load(
     cart_page.goto_inventory_standard()
     cart_page.goto_cart()
 
+    expect(cart_page.page).to_have_url(
+        "https://www.saucedemo.com/cart.html"
+    )
     expect(cart_page.continue_shopping_button).to_be_visible()
     expect(cart_page.checkout_button).to_be_visible()
     expect(cart_page.cart_list).to_be_visible()
+
+
+def test_can_logout_from_cart_page(
+    cart_page: SauceDemoCartPage
+) -> None:
+    cart_page.goto_inventory_standard()
+    cart_page.goto_cart()
+    cart_page.logout()
+
+    expect(cart_page.page).to_have_url(
+        "https://www.saucedemo.com/"
+    )
