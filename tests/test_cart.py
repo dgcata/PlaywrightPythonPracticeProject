3a4672e8b457ad4be79471e8e3f291c63b9a0b6b
dgcata <dcatacutan@fullscale.ph>
2024-04-25 18:05:24 +0800

    implemented more tests

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -1,3 +1,4 @@
+import pytest
 from playwright.sync_api import expect
 from pages.cart_page import SauceDemoCartPage
 
@@ -25,4 +26,68 @@ def test_can_logout_from_cart_page(
 
     expect(cart_page.page).to_have_url(
         "https://www.saucedemo.com/"
+    )
+
+
+def test_can_checkout_from_cart_page(cart_page: SauceDemoCartPage) -> None:
+    cart_page.goto_inventory_standard()
+    cart_page.add_item_to_cart("Sauce Labs Backpack")
+    cart_page.goto_cart()
+    cart_page.checkout()
+
+    expect(cart_page.page).to_have_url(
+        "https://www.saucedemo.com/checkout-step-one.html"
+    )
+
+
+@pytest.mark.skip("needs fixing from Sauce Labs' end")
+def test_cannot_checkout_with_empty_cart(cart_page: SauceDemoCartPage) -> None:
+    # this is a bug from Sauce Labs' end
+    # the web application can proceed to
+    # checkout even with an empty cart
+    # instead of erroring out
+    cart_page.goto_inventory_standard()
+    cart_page.goto_cart()
+    cart_page.checkout()
+
+    # assertions for erroring out
+
+
+def test_can_go_back_to_inventory(cart_page: SauceDemoCartPage) -> None:
+    cart_page.goto_inventory_standard()
+    cart_page.goto_cart()
+    cart_page.continue_shopping()
+
+    expect(cart_page.page).to_have_url(
+        "https://www.saucedemo.com/inventory.html"
+    )
+
+
+@pytest.mark.parametrize(
+    "item_name",
+    [
+        "Sauce Labs Bike Light",
+        "Sauce Labs Bolt T-Shirt",
+        "Sauce Labs Onesie",
+        "Test.allTheThings() T-Shirt (Red)",
+        "Sauce Labs Backpack",
+        "Sauce Labs Fleece Jacket",
+    ],
+)
+def test_can_remove_an_item_from_cart_page(
+    cart_page: SauceDemoCartPage, item_name: str,
+) -> None:
+    cart_page.goto_inventory_standard()
+    cart_page.add_item_to_cart(item_name)
+    cart_page.goto_cart()
+
+    # assertion before removing item
+    expect(cart_page.cart_list).to_contain_text(
+        item_name
+    )
+    # removing item
+    cart_page.remove_item_from_cart(item_name)
+    # assertion after removing item
+    expect(cart_page.cart_list).not_to_contain_text(
+        item_name
     )
