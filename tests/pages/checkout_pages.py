732cb20ae01b9484e0507e08a937f7935968ca50
dgcata <dcatacutan@fullscale.ph>
2024-04-25 22:27:31 +0800

    started implementing checkout pages

--- tests/pages/checkout_pages.py
+++ tests/pages/checkout_pages.py
@@ -1,1 +1,25 @@
-# implement Page Object Model for the checkout pages of sauce demo
+from pages.cart_page import SauceDemoCartPage
+from playwright.sync_api import Page
+
+
+class SauceDemoCheckoutPages(SauceDemoCartPage):
+    def __init__(self, page: Page) -> None:
+        super().__init__(page)
+
+        self.checkout_info = page.locator(".checkout_info")
+        self.first_name_field = page.locator('[data-test="firstName"]')
+        self.last_name_field = page.locator('[data-test="lastName"]')
+        self.postal_code_field = page.locator('[data-test="postalCode"]')
+
+        self.cancel_button = page.locator('[data-test="cancel"]')
+        self.continue_button = page.locator('[data-test="continue"]')
+        self.finish_button = page.locator('[data-test="finish"]')
+
+        self.subtotal_label = page.locator('[data-test="subtotal-label"]')
+        self.tax_label = page.locator('[data-test="tax-label"]')
+        self.total_label = page.locator('[data-test="total-label"]')
+
+        self.complete_checkout_container = page.locator(
+            '[data-test="checkout-complete-container"]'
+        )
+        self.back_to_home_button = page.locator('[data-test="back-to-products"]')
