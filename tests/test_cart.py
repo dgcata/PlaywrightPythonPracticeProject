c6cac0775b7ca82502106b40ade84492fb34c6ae
dgcata <dcatacutan@fullscale.ph>
2024-04-25 17:35:05 +0800

    implemented test_successful_load

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -1,2 +1,13 @@
-# implement tests for the cart page in isolation
-# interaction with other pages should be in test_products.py
+from playwright.sync_api import expect
+from pages.cart_page import SauceDemoCartPage
+
+
+def test_successful_load(
+    cart_page: SauceDemoCartPage
+) -> None:
+    cart_page.goto_inventory_standard()
+    cart_page.goto_cart()
+
+    expect(cart_page.continue_shopping_button).to_be_visible()
+    expect(cart_page.checkout_button).to_be_visible()
+    expect(cart_page.cart_list).to_be_visible()
